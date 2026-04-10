f8153e58a1cd7e5979cc1b597be8072b812ce8b2
Luiz Guilherme Costa <luizguilhermecsantos@gmail.com>
2026-04-10 13:53:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ require_once 'Cliente.php';
 require_once 'Produto.php';
 
 $cliente01 = new Cliente("João", "joaosilva@gmail.com");
-echo $cliente->getCliente() .PHP_EOL;
-echo $cliente->setCliente() .PHP_EOL;
+echo $cliente01->getCliente() .PHP_EOL;
+echo $cliente01->setCliente() .PHP_EOL;
 
-$produto = new Produto("Farinha", 10.00);
-echo $produto->getProduto() .PHP_EOL;
-echo $produto->getProduto() .PHP_EOL;
+$produto01 = new Produto("Farinha", 10.00);
+echo $produto01->getProduto() .PHP_EOL;
+echo $produto01->getProduto() .PHP_EOL;
 
 
