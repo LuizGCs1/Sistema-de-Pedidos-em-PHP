70a817219f4e315675b06c76c86312a09073e360
Harryson <harrysoncarvalho70@gmail.com>
2026-04-11 20:25:13 -0300

    harry commit

---
+++ Pedido.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+final class Pedido
+{
+    public function __construct(
+        private string $pedidoid,
+        private string $data
+    ) {
+    }
+}
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require_once 'Cliente.php';
 require_once 'Produto.php';
+require_once 'Pedido.php';
 
 $cliente01 = new Cliente("João", "joaosilva@gmail.com");
 echo "Cliente: " .$cliente01->getNome() .PHP_EOL;
@@ -12,5 +13,9 @@ echo "Email: " .$cliente01->getEmail() .PHP_EOL;
 $produto01 = new Produto("Farinha", 10.00);
 echo "Produto: " .$produto01->getNomeprod() .PHP_EOL;
 echo "Preço: R$ " .number_format($produto01->getPreco(), 2, ",",".").PHP_EOL;
+
+$pedido01 = new Pedido("12345", "2024-06-01");
+echo "Pedido ID: " .$pedido01->getPedidoid() .PHP_EOL;
+echo "Data do Pedido: " .$pedido01->getData() .PHP_EOL;
 
 
